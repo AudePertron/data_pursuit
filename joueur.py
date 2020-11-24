40bfd01e0f98dd0cfe6680e425c22fd2e0fea730
AudePertron <aude.pertron@isen-ouest.yncrea.fr>
2020-11-24 16:52:47 +0100

    mise à jour
    changements dans les deux fichiers

---
+++ joueur.py
@@ -0,0 +1,10 @@
+class Joueur:
+    def __init__(self, *infos):
+        self.nom = infos[0]
+        self.tour = False
+        self.camembert = [1,2,3,4,5]    #liste des thèmes, on retire chaque chiffre correspondant à un thème validé
+
+
+    def __str__(self):
+        return self.nom
+
