6928aca92ae6204462d40ee817435251b13eeb9d
AudePertron <aude.pertron@isen-ouest.yncrea.fr>
2020-11-27 14:19:54 +0100

    Update traitement_reponse.py

--- traitement_reponse.py
+++ traitement_reponse.py
@@ -1,26 +1,28 @@
 from joueur import Joueur
-from coonect_bdd import Bdd
+from connect_bdd import Bdd
 
 def traitement_reponse(reponse_joueur, id_question, joueur, theme ):    #classe/dictionnaire joueur en input
-    reponse_origine = Bdd.obtenir_reponse_id(id_question)
+
+    reponse_origine, difficulte = Bdd.obtenir_lib_dif_question(id_question) #query qui récupère réponse et difficulté
+
     if reponse_joueur == reponse_origine: #si bonne réponse
         #changer par une interface
         #utiliser query Ludivine pour choper la réponse et difficulté
         print("Réponse correcte")
         print(reponse_origine)
-        
-        statut = True
+        statut = True       #joueur.tour --> continue à jouer
         if difficulte = 3 : #si question camembert
             fonction_camembert(joueur, theme)    #appeler la fonction de gestion des camemberts. 
     else: 
-        statut = False
+        statut = False      #joueur.tour --> tour suivant
 
     return statut #booleen True or False pour continuer le tour ou passer au suivant
 
 
 def fonction_camembert(joueur, theme):        
-    joueur.camembert.append = theme   #valider le camembert de la couleur sélectionnée
-    if len(joueur.camembert >=5):
+    joueur.camembert.pop = theme   #valider le camembert de la couleur sélectionnée
+    if len(joueur.camembert < 1):
         question_finale()   #si les cinq thèmes sont validés, passer à la question finale
     return joueur
 
+def question_finale(joueur, theme)
